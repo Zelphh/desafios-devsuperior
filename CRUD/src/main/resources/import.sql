d6d18d2dc1ed371733614af5d62b1e900b4a2134
Matheus Melle Tártari <133059098+Zelphh@users.noreply.github.com>
2024-03-08 14:03:47 -0300

    Add Crud files

---
+++ CRUD/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('William Shakespeare', '608.409.045-17', 5580.00, '1964-04-23', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nicolau Maquíavel', '780.288.242-77', 7999.00, '1969-05-03', 7);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jean-Claude Van Damme', '834.180.370-46', 4890.00, '1960-10-18', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('William Blake', '375.149.598-31', 3900.00, '1957-11-28', 6);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Kurt Cobain', '263.881.328-12', 5450.00, '1967-02-20', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Friedrich Nietzsche', '662.474.834-65', 3200.00, '1944-10-15', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Linus Torvalds', '845.662.790-90', 5700.00, '1969-12-28', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Galileu Galilei', '594.589.571-76', 2850.00, '19564-02-15', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Arthur Schopenhauer', '761.352.383-70', 3500.00, '1988-02-22', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Niccolò Paganini', '517.992.651-37', 4700.00, '1982-10-27', 1);
